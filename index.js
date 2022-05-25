1762ecf2f57b767db25f57c054ab62a63499c0bd
theSaviKing <tpaulkariuki@gmail.com>
2022-05-25 17:39:25 -0500

    begin convert to function within get request

--- index.js
+++ index.js
@@ -13,54 +13,34 @@ app.get('/', function (req, res) {
 })
 
 // begin USER class definition
-class User {
-    image
-    url
-    plays
-    username
-    tracks
-    constructor(username) {
-        function getUser(username, cb) {
-            fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getinfo&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
-                .then(res => res.json())
-                .then(json => cb(json))
-        }
-        function getTracks(username) {
-            const response = fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
-            response = response.json()
-            const tracks = []
-            for (let t of response.toptracks.track) {
-                tracks.push({
-                    name: t.name,
-                    image: t.image[3]['#text'],
-                    artist: t.artist,
-                    url: t.url,
-                    duration: parseInt(t.duration),
-                    plays: parseInt(t.playcount)
-                })
-            }
-            return tracks
-        }
-        var crnt
-        getUser(username, (data) => {
-            crnt = data.user
-            console.log(`crnt=${crnt}`)
-            this.image = crnt.image[3]['#text']
-            this.url = crnt.url
-            this.username = crnt.name
-            this.plays = crnt.playcount
-        })
-        console.log(this)
-        this.dispatchEvent(objectCreated)
-    }
-}
+// `https://ws.audioscrobbler.com/2.0/?method=user.getinfo&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`
+// `https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`
+
 // end USER class definition
 
 app.get('/loading', parser, (req, res) => {
     if (Object.keys(req.query).length === 0)
         res.redirect('/')
     console.log(`Username entered: ${req.query.username}`)
-    user = new User(req.query.username)
+    var user = {}
+    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getinfo&username=${req.query.username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
+        .then(res => res.json())
+        .then(json => {
+            user.username = json.user.name
+            user.image = json.user.image[3]['#text']
+            user.plays = json.user.playcount
+            user.url = json.user.url
+            console.log(user)
+        })
+        .then(
+            res.render('results.pug', {
+                username: user['username'],
+                image: user['image'],
+                plays: user['plays'],
+                url: user['url']
+            })
+        )
+        .catch(error => res.render('error.pug'))
 })
 
 app.listen(port, function () {
