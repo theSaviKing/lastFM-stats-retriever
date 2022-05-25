d89f19a366dc0154eec16d9c5b590bbedd541649
theSaviKing <tpaulkariuki@gmail.com>
2022-05-25 16:55:42 -0500

    in progress: updating DOM with new user info

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const parser =  require("body-parser").json()
+const parser = require("body-parser").json()
 const app = express()
 const port = 8080
 const fetch = require("node-fetch")
@@ -8,90 +8,61 @@ var user;
 app.use(express.static('public'))
 app.set('views', process.cwd() + '/public')
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "/index.html"))
 })
 
 // begin USER class definition
 class User {
+    image
+    url
+    plays
+    username
+    tracks
     constructor(username) {
-        fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getinfo&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
-            .then(res => res.json())
-            .then(
-                json => this.user = json.user
-            )
-        fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
-            .then(res => res.json())
-            .then(
-                json => this.tracks = json
-            )
-        fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
-        .then(res => res.json())
-        .then(
-            json => {
-                const tracks = []
-                for(let t of json.toptracks.track) {
-                    tracks.push({
-                        name: t.name,
-                        image: t.image[3]['#text'],
-                        artist: t.artist,
-                        url: t.url,
-                        duration: parseInt(t.duration),
-                        plays: parseInt(t.playcount)
-                    })
-                }
-                this.tracks = tracks
+        function getUser(username, cb) {
+            fetch(`https://ws.audioscrobbler.com/2.0/?method=user.getinfo&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
+                .then(res => res.json())
+                .then(json => cb(json))
+        }
+        function getTracks(username) {
+            const response = fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
+            response = response.json()
+            const tracks = []
+            for (let t of response.toptracks.track) {
+                tracks.push({
+                    name: t.name,
+                    image: t.image[3]['#text'],
+                    artist: t.artist,
+                    url: t.url,
+                    duration: parseInt(t.duration),
+                    plays: parseInt(t.playcount)
+                })
             }
-        )
-    }
-
-    get image() {
-        return user.image[3]['#text']
-    }
-
-    get name() {
-        return user.name
-    }
-
-    get playcount() {
-        return user.playcount
-    }
-
-    get playlists() {
-        return user.playlists
+            return tracks
+        }
+        var crnt
+        getUser(username, (data) => {
+            crnt = data.user
+            console.log(`crnt=${crnt}`)
+            this.image = crnt.image[3]['#text']
+            this.url = crnt.url
+            this.username = crnt.name
+            this.plays = crnt.playcount
+        })
+        console.log(this)
+        this.dispatchEvent(objectCreated)
     }
 }
 // end USER class definition
-app.get('/results', parser, (req, res) => {
+
+app.get('/loading', parser, (req, res) => {
     if (Object.keys(req.query).length === 0)
         res.redirect('/')
     console.log(`Username entered: ${req.query.username}`)
-    data(req.query.username)
     user = new User(req.query.username)
-    res.render("results.pug")
 })
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`)
 })
-
-function data(username) {
-    fetch(`https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&username=${username}&api_key=b8abde52621d9cdef1158263570d1821&format=json`)
-        .then(res => res.json())
-        .then(
-            json => {
-                const tracks = [];
-                for(let t of json.toptracks.track) {
-                    tracks.push({
-                        name: t.name,
-                        image: t.image[3]['#text'],
-                        artist: t.artist,
-                        url: t.url,
-                        duration: parseInt(t.duration),
-                        plays: parseInt(t.playcount)
-                    })
-                }
-                console.log(tracks)
-            }
-        )
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const defaultTheme = require("tailwindcss/defaultTheme");
 
 module.exports = {
     content: [
-        "./**/*.html"
+        "./**/*.{html,pug}"
     ],
     theme: {
         extend: {
